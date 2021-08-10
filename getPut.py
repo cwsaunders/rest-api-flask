a2e478c8ad273bba6c1b37254d4ee7a4ee937be4
Christian Saunders <noname45688@gmail.com>
2021-08-10 18:22:10 -0400

    Update getPut.py

--- getPut.py
+++ getPut.py
@@ -104,6 +104,55 @@ def apiPut(event):
             "body": json.dumps(return_body)
         }
 
+@app.route('/create', methods=['PUT'])
+def apiPut(event):
+    if request.method == 'PUT':
+        db_name = "webforum"
+        db_port = 3306
+        db_user = 'admin'
+        db_host = 'vd1ju04cu0pert8.cusyzimfrxgh.us-east-1.rds.amazonaws.com'
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+
+        ssm_client = boto3.client('ssm')
+        response = ssm_client.get_parameter(
+            Name='web-forum-database',
+            WithDecryption=True
+        )
+        db_password = response['Parameter']['Value']
+
+
+        try:
+            conn = pymysql.connect(
+                user=db_user, password=db_password, host=db_host,
+                port=db_port, database=db_name)
+        except pymysql.MySQLError as e:
+            logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+            logger.error(e)
+            sys.exit()
+        logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+        """
+        This function PUTs content to a MySQL RDS instance
+        """
+        try:
+            body = json.loads(request.form['body'])
+            title = json.loads(request.form['title'])
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO thread (title, body) VALUES (%s, %s)", (title, body))
+            conn.commit()
+            statusCode = 200
+        except Exception as e:
+            logger.error('Fatal exception occurred.', exc_info=e)
+            statusCode = 500
+        return {
+            "statusCode": statusCode,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,PUT"
+            }
+        }
 
 
 if __name__ == '__main__':
