ddf338e679acb588afe3f45b78260c7130bd72a9
Christian Saunders <noname45688@gmail.com>
2021-08-10 18:04:26 -0400

    Update getPut.py

--- getPut.py
+++ getPut.py
@@ -5,8 +5,10 @@ import sys
 import logging
 import boto3
 from flask import Flask, render_template, request # make sure to package this in
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/', methods=['PUT', 'GET'])
