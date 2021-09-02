1333e63e15484113e826ffafce9b9f112fcf244c
Christian Saunders <noname45688@gmail.com>
2021-09-02 15:16:28 -0400

    Update getPut.py

--- getPut.py
+++ getPut.py
@@ -107,7 +107,7 @@ def apiGet():
             try:
                 with cnx.cursor() as cur:
                     cur.execute("create table if not exists thread ( id int NOT NULL AUTO_INCREMENT, title varchar(255) NOT NULL, body varchar(255) NOT NULL, PRIMARY KEY (id))")
-                    cur.commit()
+                    cnx.commit()
                     cur.execute('select * from thread limit 100')
                     return_body = [{"id": thread_id, "title": title, "body": body}
                                 for thread_id, title, body in cur]
